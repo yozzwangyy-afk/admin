a98fd9aeae43fb7253fad6b6fc73290a8f680e2e
yozzwangyy-afk <yozzwangyy@gmail.com>
2025-11-08 06:25:19 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4361ee',
+        secondary: '#3f37c9',
+      }
+    },
+  },
+  plugins: [],
+}
